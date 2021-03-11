eddf596910d0e3c9c3b375520d0f569675e928da
Mena-E <odemena.ekelemu@gmail.com>
2021-03-10 22:35:40 -0500

    Created classes in helper function.py

--- lambdata_mena2/helper_function.py
+++ lambdata_mena2/helper_function.py
@@ -1,22 +1,49 @@
 """ lambdata_mena2 - A collection of Data Science helper functions"""
 
-import pandas
-import numpy
 
-def null_count(df):
-    """Checks a dataframe for nulls and returns
-       the number of missing values.
-    """
-    return df.isna().sum().sum()
+class CleanFrame:
+    def __init__(self, df):
+        self.df = df
 
-def train_test_split(df, frac):
-    """ Splits a dataframe into a training and testing
+    def null_count(self):
+        """Checks a dataframe for nulls and returns
+           the number of missing values.
+        """
+        return self.df.isnull().sum().sum()
+
+
+class TimeSeriesSplit:
+    def __init__(self, df, frac):
+        self.df = df
+        self.frac = float(frac)
+
+    def train_test_split(self):
+        """
+        Splits a time series dataframe into a training
         and a testing set based on frac (a fraction) of
-        the percent of the size of the training data, and
-        returns the training
-    """
-    val = int(round(len(df) * frac, 0))
-    df_train = df.iloc[0:val, :]
-    df_test = df.iloc[val + 1:, :]
-    print(df_train)
-    print(df_test)
+        the percent of the size of the training data,
+        entered in the TimeSeriesSplit class.
+        :return: tuple of training and testing dataframes
+        """
+        val = int(round(len(self.df) * self.frac, 0))
+        df_train = self.df.iloc[0:val, :]
+        df_test = self.df.iloc[val + 1:, :]
+        print(df_train)
+        print(df_test)
+
+
+class RandomSplit:
+    def __init__(self, df, frac):
+        self.df = df
+        self.frac = float(frac)
+
+    def train_test_split(self):
+        """
+        Randomly splits a dataframe into a training and
+        a testing set based on frac value entered in the
+        RandomSplit class.
+        :return: tuple of training and testing dataframes.
+        """
+        df_split1 = self.df.sample(frac=self.frac)
+        df_split2 = self.df.drop(df_split1.index)
+        return (df_split1, df_split2)
---
+++ lambdata_mena2/oop_example.py
@@ -0,0 +1,88 @@
+"""Object Oriented Programming Examples - Sprint 1, Module 2"""
+# import pandas as pd
+#
+# class MyDataFrame(pd.DataFrame):
+#     def num_cells(self):
+#         return self.shape[0] * self.shape[1] # num cells of df
+
+
+class BareMinimumClass:
+    pass
+
+
+class Complex:
+    def __init__(self, real, imaginary):
+        """
+        Constructor for Complex Numbers.
+        Complex numbers have a real part, and imaginary part
+        """
+        self.r = real
+        self.i = imaginary
+
+    def add(self, other_complex):
+        self.r += other_complex.r
+        self.i += other_complex.i
+
+    def __repr__(self):
+        return "({}, {})".format(self.r, self.i)
+
+
+class SocialMediaUser:
+    def __init__(self, name, location, upvotes=0):
+        self.name = str(name)
+        self.location = location
+        self.upvotes = int(upvotes)
+
+
+    def receive_upvotes(self, num_upvotes=1):
+        self.upvotes += num_upvotes
+
+
+    def is_popular(self):
+        return self.upvotes > 100
+
+
+class Animal:
+    """ General Representation of Animals"""
+    def __init__(self, name, weight, diet_type):
+        self.name = str(name)
+        self.weight = float(weight)
+        self.diet_type = diet_type
+
+
+    def run(self):
+        return "I am flying right now"
+
+    def eat(self, food):
+        return "I love eating " + food
+
+
+class Sloth(Animal):
+
+    def __init__(self, name, weight, diet_type, num_naps=104):
+        super().__init__(name, weight, diet_type)
+        self.num_naps = float(num_naps)
+
+    def eat(self, food):
+        return "I really love eating"
+
+
+if __name__ == "__main__":
+    num1 = Complex(3, -5) # num1.r =3, num1.i = -5
+    num2 = Complex(2, 6) # num2.r = 2, num2.i = 6
+
+    num1.add(num2)
+    print("num1.r: {}, num1.i: {}".format(num1.r, num1.i))
+
+    user1 = SocialMediaUser("Mena", "Boston", 300)
+    user2 = SocialMediaUser("John", "New York", 88)
+    user3 = SocialMediaUser("Alex", "Cambridge", 2000)
+    user4 = SocialMediaUser("Carl", "lagos", 99)
+
+    print(user1.is_popular())
+    print(user2.is_popular())
+    user2.receive_upvotes(25)
+    print(user2.is_popular())
+    print("Received {} upvotes".format(user2.upvotes))
+    print(user2.is_popular())
+
